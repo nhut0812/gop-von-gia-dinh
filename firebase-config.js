5a88cf3ed9dfb2cf64eb27a7a7eddee4ea75c254
nhut0812 <nvnhut123@gmail.com>
2026-01-10 20:50:32 +0700

    Add Firebase config for online data sync

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBEukgwiPZ3ZyNzt8hPtD9guJCmD_rDGWc",
+    authDomain: "gop-von-gia-dinh.firebaseapp.com",
+    projectId: "gop-von-gia-dinh",
+    storageBucket: "gop-von-gia-dinh.firebasestorage.app",
+    messagingSenderId: "242858400604",
+    appId: "1:242858400604:web:83b5b9d43fa1818cbe3f99"
+};
+
+window.firebaseConfig = firebaseConfig;
